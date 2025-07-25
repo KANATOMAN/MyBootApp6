6bf8439a0b02d7a1951967d38975c6302296b80e
KANATOMAN <jcte2a06@gmail.com>
2025-07-25 15:57:18 +0900

    aiue

--- src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp6/mybootapp6/BookController.java
@@ -2,35 +2,51 @@ package jp.te4a.spring.boot.myapp6.mybootapp6;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BookController {
+
     @Autowired
- BookService bookService;
-@RequestMapping("/")
- public String index(Model model) {
- model.addAttribute("msg","this is setting message");
- return "index";
- }
- @RequestMapping(value="/post", method=RequestMethod.POST)
-public ModelAndView postForm(@RequestParam("id") String id, 
- @RequestParam("title") String title,@RequestParam("writter") String writter, 
- @RequestParam("publisher") String publisher,@RequestParam("price") String price) {
- ModelAndView mv = new ModelAndView("index");
- bookService.save(new BookBean(Integer.valueOf(id), title, writter, publisher, Integer.valueOf(price)));
- StringBuffer buff = new StringBuffer();
- buff.append("<HR>");
- for(BookBean bean:bookService.findAll()) {
- buff.append("ID:" + bean.getId() + "<BR>“ + "タイトル:" + bean.getTitle() + 
- "<BR>"+ "著者:" + bean.getWritter() + "<BR>" + "出版社:" + bean.getPublisher() + 
- “<BR>”+ “価格:” + bean.getPrice() + “<BR><HR>");
- }
- mv.addObject("msg", buff.toString());
- return mv;
- }
+    BookService bookService;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("msg", "this is setting message");
+        return "index";  // templates/index.html を返す（Thymeleafなど使用時）
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST)
+    public ModelAndView postForm(
+        @RequestParam("id") String id,
+        @RequestParam("title") String title,
+        @RequestParam("writter") String writter,
+        @RequestParam("publisher") String publisher,
+        @RequestParam("price") String price
+    ) {
+        ModelAndView mv = new ModelAndView("index");
+
+        // 入力データを保存
+        bookService.save(new BookBean(Integer.valueOf(id), title, writter, publisher, Integer.valueOf(price)));
+
+        // データ表示用HTMLを生成
+        StringBuilder buff = new StringBuilder();
+        buff.append("<HR>");
+        for (BookBean bean : bookService.findAll()) {
+            buff.append("ID: ").append(bean.getId()).append("<BR>")
+                .append("タイトル: ").append(bean.getTitle()).append("<BR>")
+                .append("著者: ").append(bean.getWritter()).append("<BR>")
+                .append("出版社: ").append(bean.getPublisher()).append("<BR>")
+                .append("価格: ").append(bean.getPrice()).append("<BR><HR>");
+        }
+
+        mv.addObject("msg", buff.toString());
+        return mv;
+    }
 }
-    
-}
+
+
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,25 +1,45 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
- pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" 
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
-"http://www.w3.org/TR/html4/loose.dtd">
+    "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Index</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>図書登録</title>
 </head>
 <body>
-    <h1>Index Page</h1>
-    <p>this is sample page for Spring Boot!</p>
-    <p>${msg}</p>
+    <h1>図書登録フォーム</h1>
+    <p>このページはSpring Bootのサンプルです。</p>
+
+    <!-- メッセージ表示 -->
+    <p><%= request.getAttribute("msg") %></p>
+
+    <!-- 登録フォーム -->
     <form method="post" action="/post">
-    <table>
-    <tr><td>ID:</td><td><input type=“text” name=“id” /></td></tr>
-    <tr><td>タイトル:</td><td><input type="text" name="title“ /></td></tr>
-    <tr><td>著者：</td><td><input type="text" name="writter“ /></td></tr>
-    <tr><td>出版社：</td><td><input type="text" name="publisher“ /></td></tr>
-    <tr><td>価格：</td><td><input type="text" name="price“ /></td></tr>
-    </table>
-    <input type="submit“ />
+        <table border="0" cellpadding="5">
+            <tr>
+                <td>ID<span style="color:red">*</span>：</td>
+                <td><input type="text" name="id" required /></td>
+            </tr>
+            <tr>
+                <td>タイトル：</td>
+                <td><input type="text" name="title" /></td>
+            </tr>
+            <tr>
+                <td>著者：</td>
+                <td><input type="text" name="writter" /></td>
+            </tr>
+            <tr>
+                <td>出版社：</td>
+                <td><input type="text" name="publisher" /></td>
+            </tr>
+            <tr>
+                <td>価格：</td>
+                <td><input type="text" name="price" /></td>
+            </tr>
+        </table>
+        <br>
+        <input type="submit" value="登録" />
     </form>
-   </body>
-   </html>
+</body>
+</html>
